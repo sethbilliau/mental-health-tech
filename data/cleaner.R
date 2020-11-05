3017bb60ae884122909e8c3b733358a27a35efec
Seth Billiau <sethbilliau@Seths-MacBook-Pro.local>
2020-11-05 18:04:34 -0500

    16

--- data/cleaner.R
+++ data/cleaner.R
@@ -22,6 +22,8 @@ colnames(dat18) = gsub("<strong>", "", colnames(dat18))
 colnames(dat18) = gsub("</strong>", "", colnames(dat18))
 colnames(dat17) = gsub("<strong>", "", colnames(dat17))
 colnames(dat17) = gsub("</strong>", "", colnames(dat17))
+colnames(dat16) = gsub("<strong>", "", colnames(dat16))
+colnames(dat16) = gsub("</strong>", "", colnames(dat16))
   
 # Get common columns 
 commoncols.1819 = colnames(dat19)[which(colnames(dat19) %in% colnames(dat18))]
@@ -44,7 +46,7 @@ names(df1819) = commoncols.1819
 # Get common columns 
 commoncols.full = colnames(df1819)[which(colnames(df1819) %in% colnames(dat17))]
 commoncols.idx.full = which(colnames(df1819) %in% colnames(dat17))
-n = length(c(unlist(dat17[,1]), unlist(dat1819[,1])))
+n = length(c(unlist(dat17[,1]), unlist(df1819[,1])))
 
 # Build New dataset 
 df1719 = data.frame(matrix(ncol = length(commoncols.idx.full), nrow = n))
@@ -61,10 +63,69 @@ View(df1719)
 
 # Df 17-19 write 
 write.csv(df1719, 'df17_19.csv')
+ 
 
+barplot(table(as.factor(df1719$`What is your race?`)))
+df_race = data.frame(x=table(as.factor(df1719$`What is your race?`)))
 
-
-
-
+ggplot(df_race, aes(x=x.Var1, y = x.Freq)) + 
+  geom_bar(stat="identity", color="blue", fill="white") +
+  labs(title = "Posterior Correlation X1 and X2") +
+  theme(plot.title = element_text(hjust = 0.5)) 
+gender = df1719$`What is your gender?`
+gender[which(grepl("female", gender))] = 'F'
+gender[which(grepl("Female", gender))] = 'F'
+gender[which(grepl("woman", gender))] = 'F'
+gender[which(grepl("Woman", gender))] = 'F'
+gender[which(grepl("f", gender))] = 'F'
+gender[which(grepl("male", gender))] = 'M'
+gender[which(grepl("Male", gender))] = 'M'
+gender[which(grepl("man", gender))] = 'M'
+gender[which(grepl("Man", gender))] = 'M'
+gender[which(grepl("m", gender))] = 'M'
+gender[which(grepl("MALE", gender))] = 'M'
+gender[which(grepl("SWM", gender))] = 'M'
+gender[which( gender != "M" & gender != "F")] = 'Other'
+table(gender)
+df1719$`What is your gender?` = as.factor(gender)
 
      
+write.csv(df1719, 'df17_19.csv')
+
+
+
+### Add 16
+
+# Get common columns 
+commoncols.add16 = colnames(df1719)[which(colnames(df1719) %in% colnames(dat16))]
+commoncols.idx.add16 = which(colnames(df1719) %in% colnames(dat16))
+n = length(c(unlist(dat16[,1]), unlist(df1719[,1])))
+
+# Build New dataset 
+df1619 = data.frame(matrix(ncol = length(colnames(df1719)), nrow = n))
+for (i in 1:length(commoncols.add16)) { 
+  idx.19 = commoncols.idx.add16[i]
+  name = colnames(df1719)[idx.19]
+  idx.16 = which(colnames(dat16) == name)
+  newcol = c(unlist(dat16[,idx.16]), unlist(df1719[,idx.19]))
+  df1619[,i] = numeric(length(newcol))
+  df1619[,i] = newcol
+}
+colnames(df1619)[1:length(commoncols.add16)] = commoncols.add16
+
+
+df1619[1805,]
+uncommoncols.add16 = colnames(df1719)[which(!(colnames(df1719) %in% colnames(dat16)))]
+uncommoncols.idx.add16 = which(!(colnames(df1719) %in% colnames(dat16)))
+
+for (i in 1:length(uncommoncols.add16)) { 
+  idx.19 = uncommoncols.idx.add16[i]
+  name = colnames(df1719)[idx.19]
+  newcol = c(rep(NA, length(unlist(dat16[,1]))), unlist(df1719[,idx.19]))
+  df1619[,(i+length(commoncols.add16))] = numeric(length(newcol))
+  df1619[,(i+length(commoncols.add16))] = newcol
+}
+colnames(df1619)[(length(commoncols.add16) + 1):(length(commoncols.add16) + length(uncommoncols.add16))] = uncommoncols.add16
+
+
+df1619$`What is your race?`[1809:2000]
