c86044c15decd5bb591b02ca79a75b3a93a93039
Seth Billiau <sethbilliau@Seths-MacBook-Pro.local>
2020-11-08 14:57:29 -0500

    seth update

--- data/cleaner.R
+++ data/cleaner.R
@@ -227,6 +227,28 @@ df =df1619[which(!is.na( df1619[,options[1] ])), ]
 df$year
 
 
+gender = df1619$`What is your gender?`
+gender[which(grepl("female", gender))] = 'F'
+gender[which(grepl("Female", gender))] = 'F'
+gender[which(grepl("woman", gender))] = 'F'
+gender[which(grepl("Woman", gender))] = 'F'
+gender[which(grepl("f", gender))] = 'F'
+gender[which(grepl("male", gender))] = 'M'
+gender[which(grepl("Male", gender))] = 'M'
+gender[which(grepl("man", gender))] = 'M'
+gender[which(grepl("Man", gender))] = 'M'
+gender[which(grepl("m", gender))] = 'M'
+gender[which(grepl("MALE", gender))] = 'M'
+gender[which(grepl("SWM", gender))] = 'M'
+gender[which( gender != "M" & gender != "F")] = 'Other'
+table(gender)
+df1619$`What is your gender?` = as.factor(gender)
+
+
+write.csv(df1619, 'df16_19_clean.csv')
+
+
+
 
 
 
---
+++ data/manuallycleaned/cleaner2.R
@@ -0,0 +1,7 @@
+# CLEANER Manually augmented
+
+# Libraries
+library(tidyverse)
+
+dat = read_csv("manual_1619.csv")
+dat$`What is your gender?`
