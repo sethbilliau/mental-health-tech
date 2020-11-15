7565eceae3906cbb408ec86be5ecec2f9e5eac5d
Austin Hwang <austin.y.hwang@gmail.com>
2020-11-15 11:59:11 -0800

    initial line chart

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # mental-health-tech
+## Data sources
+- https://siblisresearch.com/data/us-sector-weightings/
+- https://www.bespokepremium.com/search/sector+weightings/
--- js/main.js
+++ js/main.js
@@ -1,1 +1,20 @@
-console.log("let's get started!")
+let lineGraphVis;
+
+// let promises = [
+//     d3.csv("data/sp.csv")
+// ]
+
+// Promise.all(promises)
+// .then(function (data) {
+//     initMainPage(data)
+// })
+// .catch(function (err) {
+//     console.log(err);
+// });
+d3.csv('data/sp.csv', function(data) {
+    initMainPage(data);
+})
+
+function initMainPage(data) {
+    lineGraphVis = new LineGraph("lineGraphDiv", data);
+}
