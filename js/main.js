ac61e5d61c397f531d94c674a8fc8a0514a78786
Seth Billiau <sethbilliau@Seths-MacBook-Pro.local>
2020-11-15 14:19:11 -0500

    seth pushes map and word cloud

--- js/main.js
+++ js/main.js
@@ -1,1 +1,43 @@
 console.log("let's get started!")
+
+/* * * * * * * * * * * * * *
+*           MAIN           *
+* * * * * * * * * * * * * */
+
+// init global variables & switches
+let myMapVis;
+
+
+// load data using promises
+let promises = [
+
+    d3.json("https://cdn.jsdelivr.net/npm/us-atlas@3/states-10m.json"),  // not projected -> you need to do it
+    // d3.json("https://cdn.jsdelivr.net/npm/us-atlas@3/states-albers-10m.json"), // already projected -> you can just scale it to ft your browser window
+    d3.csv("data/df_1619_colnames.csv")
+];
+
+Promise.all(promises)
+    .then( function(data){ initMainPage(data) })
+    .catch( function (err){console.log(err)} );
+
+// initMainPage
+function initMainPage(dataArray) {
+
+    // log data
+    console.log('check out the data', dataArray);
+
+    // Map Vis
+    myMapVis = new MapVis('map', dataArray[0], dataArray[1]);
+
+    myCloudVis = new WordCloudVis('cloudDiv', dataArray[1]);
+
+
+}
+
+function categoryChange() {
+    selectedCategory = $('#categorySelector').val();
+    myMapVis.wrangleData(); // maybe you need to change this slightly depending on the name of your MapVis instance
+}
+
+
+
