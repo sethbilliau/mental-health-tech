5328b0fd9c8a20ef3bb30c7b1cb431ed22cc443a
sarahjh31 <sarahhong@college.harvard.edu>
2020-11-22 15:51:00 -0500

    finished story

--- js/bubble.js
+++ js/bubble.js
@@ -670,18 +670,18 @@ class BubbleChart {
                 console.log('LOOK AT ME HERE')
                 $(`#step${vis.step+1}`).html(`<h5><br><strong>41%</strong> of all respondents said they are struggling with mental health issues.</h5><h6>Hover to see what mental health disorders they are struggling with.</h6>`);
             }
-            // if(vis.step == 1){
-            //     $(`#step${vis.step+1}`).html(`<h5>A <strong>higher proportion</strong> of people who identified as <strong>Females</strong> and <strong>Other</strong> are struggling with mental health issues, compared to Males.</h5><h6>Hover to see what mental health disorders each group is struggling with.</h6>`);
-            // }
-            // if(vis.step == 2){
-            //     $(`#step${vis.step+1}`).html(`<h5>Test ${vis.step+1}</h5>`);
-            // }
-            // if(vis.step == 3){
-            //     $(`#step${vis.step+1}`).html(`<h5>Text ${vis.step+1}</h5>`);
-            // }
-            // else{
-            //     $(`#step${vis.step+1}`).html(`<h5>Text ${vis.step+1}</h5>`);
-            // }
+            else if(vis.step == 1){
+                $(`#step${vis.step+1}`).html(`<h5>A <strong>higher proportion</strong> of people who identified as <strong>Females</strong> and <strong>Other</strong> are struggling with mental health issues, compared to Males.</h5><h6>Hover to see what mental health disorders each group is struggling with.</h6>`);
+            }
+            else if(vis.step == 2){
+                $(`#step${vis.step+1}`).html(`<h5>A <strong>higher proportion</strong> of <strong>younger people</strong> reported they were struggling with a mental health issue than older people.</h5><h6>Hover to see what mental health disorders each group is struggling with.</h6>`);
+            }
+            else if(vis.step == 3){
+                $(`#step${vis.step+1}`).html(`<h5>A <strong>higher proportion</strong> of <strong>younger people</strong> reported they were struggling with a mental health issue than older people.</h5><h6>Hover to see what mental health disorders each group is struggling with.</h6>`);
+            }
+            else{
+                $(`#step${vis.step+1}`).html(`<h5><strong>Designers</strong> had the highest proportion of mental health issues. <strong>Developers</strong> had the lowest proportion.</h5><h6>Hover to see what mental health disorders each group is struggling with.</h6>`);
+            }
            
 
         }
