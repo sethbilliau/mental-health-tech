6b73dc64d55d3678468afa30920d453929c6db90
Austin Hwang <austin.y.hwang@gmail.com>
2020-11-21 16:46:54 -0800

    add last timeline

--- js/doubleLine.js
+++ js/doubleLine.js
@@ -26,11 +26,31 @@ class DoubleLine {
             .append("g")
             .attr("transform", "translate(" + vis.margin.left + "," + vis.margin.top + ")");
 
-        vis.parseTime = d3.timeParse("%Y");
+        vis.parseYear = d3.timeParse("%Y");
 
-        vis.x = d3.scaleTime().range([0, vis.width]);
-        vis.y0 = d3.scaleLinear().range([vis.height, 0]);
-        vis.y1 = d3.scaleLinear().range([vis.height, 0]);
+        vis.x = d3.scaleTime()
+            .range([0, vis.width]);
+
+        vis.y = d3.scaleLinear()
+            .domain([0, 50])
+            .range([vis.height, 0]);
+
+        vis.xAxis = d3.axisBottom()
+            .scale(vis.x)
+            .ticks(4);
+
+        vis.yAxis = d3.axisLeft()
+            .scale(vis.y)
+            .tickFormat(d => d + "%")
+            .ticks(5);
+
+        vis.svg.append("clipPath")
+            .attr("id", "clip")
+            .append("rect")
+            .attr("width", vis.width)
+            .attr("height", vis.height);
+
+
 
         this.wrangleData();
     }
@@ -38,21 +58,119 @@ class DoubleLine {
     wrangleData() {
         let vis = this;
 
-        vis.displayData = {
-            "has_disorder": {
-                "2016": 0,
-                "2017": 0,
-                "2018": 0,
-                "2019": 0
+        vis.displayData = [{
+                year: "2016",
+                count: 0,
+            },
+            {
+                year: "2017",
+                count: 0
+            },
+            {
+                year: "2018",
+                count: 0,
+            },
+            {
+                year: "2019",
+                count: 0,
             }
-            
-        }
+        ]
 
-        console.log(vis.surveyData)
+        let totalCount = {
+            "2016": 0,
+            "2017": 0,
+            "2018": 0,
+            "2019": 0
+        }
+        vis.surveyData.forEach(el => {
+            let disorder = true ? el.disorder === "Yes" : false;
+            totalCount[el.year] += 1;
+
+            if (el.year === "2016") {
+                vis.displayData[0].count += disorder;
+            } else if (el.year === "2017") {
+                vis.displayData[1].count += disorder;
+            } else if (el.year === "2018") {
+                vis.displayData[2].count += disorder;
+            } else {
+                vis.displayData[3].count += disorder;
+            }
+        })
+
+        vis.displayData.forEach(el => {
+            el.count = Math.round((el.count / totalCount[el.year]) * 100);
+        })
+
+        console.log(vis.displayData)
         this.updateVis();
     }
 
     updateVis() {
         let vis = this;
+        vis.x.domain(d3.extent(vis.displayData, d => vis.parseYear(d.year)))
+
+        // const tooltip = d3.select("body").append("div")
+        //     .attr("class", "tooltip-line")
+        //     .style("opacity", 0)
+        //     .style("position", "absolute");
+
+
+        let line = d3.line()
+            .x(d => vis.x(vis.parseYear(d.year)))
+            .y(d => vis.y(parseFloat(d.count)));
+
+
+        vis.svg.append("path")
+            .datum(vis.displayData)
+            .attr('clip-path', 'url(#clip)')
+            .attr("fill", "none")
+            .style("stroke", "red")
+            .style("stroke-width", "2")
+            .attr("d", line);
+
+
+        vis.svg.append("clipPath")
+            .attr("id", "clip")
+            .append("rect")
+            .attr("width", vis.width)
+            .attr("height", vis.height);
+
+        // Add the x-axis.
+        vis.svg.append("g")
+            .attr("class", "x axis")
+            .attr("transform", "translate(0," + vis.height + ")")
+            .call(vis.xAxis);
+
+        // Add the y-axis.
+        vis.svg.append("g")
+            .attr("class", "y axis")
+            .call(vis.yAxis);
+
+        vis.svg.selectAll(".dot")
+            .data(vis.displayData)
+            .enter().append("circle")
+            .attr("class", "dot")
+            .attr("cx", d => vis.x(vis.parseYear(d.year)))
+            .attr("cy", d => vis.y(parseFloat(d.count)))
+            .attr("r", 5)
+            .attr("fill", "red");
+
+        let curtain = vis.svg.append('rect')
+            .attr('x', -1 * vis.width - 5)
+            .attr('y', -1 * vis.height)
+            .attr('height', vis.height)
+            .attr('width', vis.width)
+            .attr('class', 'curtain')
+            .attr('transform', 'rotate(180)')
+            .style('fill', '#ffffff')
+
+        let t = vis.svg.transition()
+            .delay(750)
+            .duration(6000)
+            .ease(d3.easeLinear);
+
+        t.select('rect.curtain')
+            .attr('width', 0);
+
     }
 }
